c6f4a30f3be964033aa743f73e863fa07f9f95d6
jun-yuaka <yuaka0511@gmail.com>
2023-10-25 06:10:47 +0900

    stackNavigatorの導入

--- App.js
+++ App.js
@@ -1,67 +1,16 @@
-import { useState, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, SafeAreaView, FlatList } from 'react-native';
-import { ListItem } from './components/ListItem'
-import Constants from 'expo-constants';
-import axios from 'axios';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HomeScreen } from './screens/HomeScreen';
 
-const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.expoConfig.extra.newsApiKey}`
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [articles, setArticles] = useState([]);
-
-  const fetchArticles = async() => {
-    try {
-      const response = await axios.get(URL)
-      setArticles(response.data.articles);
-    }catch(error) {
-      console.error(error)
-    }
-  };
-
-  useEffect(() => {
-    fetchArticles();
-  }, []);
-
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={articles}
-        renderItem={({ item }) => (
-          <ListItem
-            imageUrl={item.urlToImage}
-            title={item.title}
-            author={item.author}
-          />
-        )}
-        keyExtractor={(item, index) => index.toString()}
-      />
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+ );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#eee',
-  },
-  itemContainer: {
-    height: 100,
-    width: "100%",
-    backgroundColor: "white",
-    flexDirection: "row",
-  },
-  leftContainer: {
-    width: 100,
-    backgroundColor: "green",
-  },
-  rightContainer: {
-    flex: 1,
-    padding: 10,
-    justifyContent: "space-between",
-  },
-  subText: {
-    color: "gray",
-  },
-});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,65 @@
+import { useState, useEffect } from 'react';
+import { StyleSheet, SafeAreaView, FlatList } from 'react-native';
+import { ListItem } from '../components/ListItem'
+import Constants from 'expo-constants';
+import axios from 'axios';
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.expoConfig.extra.newsApiKey}`
+
+export const HomeScreen = () => {
+  const [articles, setArticles] = useState([]);
+
+  const fetchArticles = async() => {
+    try {
+      const response = await axios.get(URL)
+      setArticles(response.data.articles);
+    }catch(error) {
+      console.error(error)
+    }
+  };
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#eee',
+  },
+  itemContainer: {
+    height: 100,
+    width: "100%",
+    backgroundColor: "white",
+    flexDirection: "row",
+  },
+  leftContainer: {
+    width: 100,
+    backgroundColor: "green",
+  },
+  rightContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "space-between",
+  },
+  subText: {
+    color: "gray",
+  },
+});
